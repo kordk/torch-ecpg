d939a88980eb7794672d76405d01c87c1da8dc23
unknown <liam.g.berger@gmail.com>
2022-07-12 11:33:43 -0700

    Generalized the cpu compute function

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
-from typing import List, Tuple
+from typing import Any, Callable, Dict, Tuple
 import pandas
 from scipy.stats import pearsonr
 from samle_data import download_dataframes
 from config import DATA_DIR
 
 
-def calc_pearsonr(
-    M: pandas.DataFrame, G: pandas.DataFrame
-) -> List[Tuple[str, str, float, float]]:
+def compute_cpu(
+    M: pandas.DataFrame,
+    G: pandas.DataFrame,
+    calculate_func: Callable[[Any, Any], Any] = pearsonr,
+) -> Dict[Tuple[str, str], Any]:
     M = M.reindex(sorted(M.columns), axis=1)
     G = G.reindex(sorted(G.columns), axis=1)
 
-    out = []
+    out = {}
     for m_label, m_row in M.iteritems():
         for g_label, g_row in G.iteritems():
-            corr, p_val = pearsonr(m_row.values, g_row.values)
-            out.append((m_label, g_label, corr, p_val))
+            m_values, g_values = m_row.values, g_row.values
+            res = calculate_func(m_values, g_values)
+            out[m_label, g_label] = res
     return out
 
 
@@ -23,7 +26,7 @@ def main() -> None:
     dataframes = download_dataframes(DATA_DIR)
     M = dataframes['M.csv']
     G = dataframes['G.csv']
-    print(calc_pearsonr(M, G))
+    print(compute_cpu(M, G))
 
 
 if __name__ == '__main__':
