49328e8bf03cf49da898d6eb1027adc808732ca8
unknown <liam.g.berger@gmail.com>
2022-07-17 13:06:10 -0700

    Added pearson tensor corr chunking

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from time import time
-
 import torch
 from import_data import download_dataframes
 from config import WORKING_DATA_DIR
-from pearson_full import pearson_full_tensor
+from pearson_full import pearson_chunk_tensor
 
 
 def main() -> None:
@@ -13,9 +12,9 @@ def main() -> None:
 
     torch.cuda.empty_cache()
     start = time()
-    pearson_full_tensor(M, G)
+    pearson_chunk_tensor(M, G, 10000, verbose=True)
     t1 = time() - start
-    print(f'Full tensor corr time: {t1} seconds')
+    print(f'Chunk tensor corr time: {t1} seconds')
 
 
 if __name__ == '__main__':
--- src/pearson_full.py
+++ src/pearson_full.py
@@ -26,14 +26,7 @@ def pearson_full_tensor(
     With the iterative formula above, the means are calculated (n*k)
     times for x and y. With the full solution, the means are calculated
     (n+m) times. The same is true for stds.
-    '''
-    if n is None:
-        n = len(M.columns)
 
-    M_t = torch.tensor(M.to_numpy()).to(device)
-    G_t = torch.tensor(G.to_numpy()).to(device)
-
-    '''
     # The code below is the expanded version of the function. While the
     # code is more readable in this form, it runs into CUDA memory
     # issues if the input is too large.
@@ -56,6 +49,11 @@ def pearson_full_tensor(
         columns=G.index,
     )
     '''
+    if n is None:
+        n = len(M.columns)
+
+    M_t = torch.tensor(M.to_numpy()).to(device)
+    G_t = torch.tensor(G.to_numpy()).to(device)
 
     corr_pd = pandas.DataFrame(
         (
@@ -65,5 +63,105 @@ def pearson_full_tensor(
         index=M.index,
         columns=G.index,
     )
+
+    return corr_pd
+
+
+def pearson_chunk_tensor(
+    M: pandas.DataFrame,
+    G: pandas.DataFrame,
+    chunks: int,
+    n: Optional[int] = None,
+    verbose: bool = False,
+) -> pandas.DataFrame:
+    '''
+    Utilizes chunks of values to avoid running into GPU memory limits.
+    The chunk version is often faster than the full version by a small
+    amount. Adjust chunks such that torch is not using too much GPU
+    memory.
+
+    Chunks is the number of chunks into which to divide the M dataframe.
+    Verbose prints messages to gauge time.
+
+    Calculates the pearson correlation coefficient matrix for two full
+    dataframes. Returns a dataframe of the output with M.index indices
+    and G.index columns. If n, the sample size, is not provided, it will
+    infer it from len(M.columns).
+
+    Calculating the pearson correlation coefficient matrix given the
+    entire two matrices is monumentally faster than single iteration
+    pearson.
+
+    The algorithm works by removing much of the unneeded recomputation
+    present in pearson_corr_tensor. The algorithm is roughly based on
+    ((x . y) - n * mean(x) * mean(y)) / ((n - 1) * std(x) * std(y)).
+
+    n = M rows
+    k = G rows
+    With the iterative formula above, the means are calculated (n*k)
+    times for x and y. With the full solution, the means are calculated
+    (n+m) times. The same is true for stds.
+
+    # The code below is the expanded version of the function. While the
+    # code is more readable in this form, it runs into CUDA memory
+    # issues if the input is too large.
+
+    M_t = torch.tensor(M.to_numpy()).to(device)
+    G_t = torch.tensor(G.to_numpy()).to(device)
+    G_means = G_t.mean(axis=1)
+    G_std = G_t.std(axis=1)
+    corr_pd = pandas.DataFrame()
+    for index, i in enumerate(range(0, len(M_t), chunk_rows), 1):
+        if verbose:
+            print(f'Chunk {index}/{len(M.index) // chunk_rows}')
+        M_chunk = M_t[i : i + chunk_rows]
+        M_chunk_means = M_chunk.mean(axis=1)
+        M_chunk_std = M_chunk.mean(axis=1)
+        dots = M_chunk @ G_t.T
+        sub = M_chunk_means.outer(G_means)
+        sub *= n
+        denom = (M_chunk_std.outer(G_std)
+        denom *= n - 1
+        corr_t_chunk = (dots - sub) / denom
+        corr_pd_chunk = pandas.DataFrame(
+            corr_t_chunk.tolist(),
+            index=M.index[i : i + chunk_rows],
+            columns=G.index,
+        )
+        corr_pd = corr_pd.append(corr_pd_chunk)
+        del M_chunk
+        del corr_t_chunk
+    '''
+    if n is None:
+        n = len(M.columns)
+    chunk_rows = len(M) // chunks
+    if verbose:
+        print(
+            'Running pearson chunk tensor with'
+            f' {chunk_rows * len(G) * n} values per chunk ({chunk_rows} rows'
+            ' per chunk).'
+        )
+
+    M_t = torch.tensor(M.to_numpy()).to(device)
+    G_t = torch.tensor(G.to_numpy()).to(device)
+    G_means = G_t.mean(axis=1)
+    G_std = G_t.std(axis=1)
+
+    corr_pd = pandas.DataFrame()
+    for index, i in enumerate(range(0, len(M_t), chunk_rows), 1):
+        if verbose:
+            print(f'Chunk {index}/{len(M.index) // chunk_rows}')
+        M_chunk = M_t[i : i + chunk_rows]
+        corr_t_chunk = (
+            M_chunk @ G_t.T - M_chunk.mean(axis=1).outer(G_means) * n
+        ) / (M_chunk.std(axis=1).outer(G_std) * (n - 1))
+        corr_pd_chunk = pandas.DataFrame(
+            corr_t_chunk.tolist(),
+            index=M.index[i : i + chunk_rows],
+            columns=G.index,
+        )
+        corr_pd = corr_pd.append(corr_pd_chunk)
+        del M_chunk
+        del corr_t_chunk
 
     return corr_pd
