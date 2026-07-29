377ad31fe7db27b15f3fca9ea41f8505e900f241
kordk <kord.kober@gmail.com>
2026-07-28 18:34:07 -0700

    Handle numpy boolean type in JSON encoder

--- tools/benchmark_kennedy.py
+++ tools/benchmark_kennedy.py
@@ -731,6 +731,8 @@ def write_provenance_and_reports(args, num_merged, diag_results, grid_results, d
     class NpEncoder(json.JSONEncoder):
         def default(self, obj):
             import numpy as np
+            if isinstance(obj, np.bool_):
+                return bool(obj)
             if isinstance(obj, np.integer):
                 return int(obj)
             if isinstance(obj, np.floating):
